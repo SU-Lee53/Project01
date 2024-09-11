b7ff956e5f710111dc98152469a171fd2d252b39
SU-Lee53 <dltmddnr05@naver.com>
2024-09-12 06:29:51 +0900

    Engine project separation

---
+++ Engine/pch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// Types
+#include "Types.h"
+
+// STL
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <memory>
+
+using namespace std;
+
+// OPENGL
+#pragma comment (lib, "freeglut/freeglut.lib")
+#pragma comment (lib, "glew/glew32.lib")
+
+#include <gl/glew.h>
+#include <freeglut/freeglut.h>
+#include <freeglut/freeglut_ext.h>
+// glm types are included in Types.h
+
+
+// PreCompile
+#include "Game.h"
--- Game/pch.h
+++ Game/pch.h
@@ -1,26 +1,5 @@
 #pragma once
 
-// Types
-#include "Types.h"
-
-// STL
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <functional>
-#include <memory>
-
-using namespace std;
-
-// OPENGL
-#pragma comment (lib, "freeglut/freeglut.lib")
-#pragma comment (lib, "glew/glew32.lib")
-
-#include <gl/glew.h>
-#include <freeglut/freeglut.h>
-#include <freeglut/freeglut_ext.h>
-// glm types are included in Types.h
-
-
-// PreCompile
-#include "Game.h"
+// Engine
+#pragma comment (lib, "Engine/Engine.lib")
+#include "Engine/pch.h"
